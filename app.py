d5661771dd571af1506d83d1351dde5e4abfcd2c
idanbau <81595179+idanbau@users.noreply.github.com>
2022-08-21 17:09:32 +0300

    TELEGRAM READY FOR PRODUCTION BOT

--- app.py
+++ app.py
@@ -1,36 +1,32 @@
 import io
 
+import waitress
+
+import config
 from flask import Flask, Response, request
 import face_recognition
 import requests
 import numpy as np
 from PIL import Image
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
 ACCEPT_MESSAGE = "אני 052-538-164-8"
 DECLINE_MESSAGE = "!מי זאת"
 
 app = Flask(__name__)
-TOKEN = '5696168580:AAHuXW8Jd0lZjYROK4cgpPEfEsOXhOxbEOE'
-NGROK = 'https://c0a8-82-80-173-170.eu.ngrok.io'
+app.config['SECRET_KEY'] = config.SECRET_KEY
+TOKEN = 'TELEGRAM BOT TOKEN'
+NGROK = 'NGROK ADDRESS'
 TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url={}/message'.format(TOKEN, NGROK)
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
 
-
-@app.route('/sanity')
-def sanity(): return "Server is running"
-
-
 @app.route('/message', methods=["POST"])
 def handle_message():
-    print("got message")
     json = request.get_json()
-    print(json)
+    if 'message' not in json:
+        return Response("failure")
     chat_id = json['message']['chat']['id']
     if 'photo' not in json['message']:
-        res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'".format(TOKEN, chat_id,
-                                                                                                   DECLINE_MESSAGE))
+        res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TOKEN, chat_id,                                                                                                   DECLINE_MESSAGE))
         return Response("failure")
     message = json['message']['photo']
     pic_file_path = requests.get('https://api.telegram.org/bot{}/getfile?file_id={}'.format(TOKEN, message[0]['file_id'])).json()['result']['file_path']
@@ -41,11 +37,11 @@ def handle_message():
     unknown_faces = face_recognition.face_encodings(pic)
     match_results = [unknown_face for unknown_face in unknown_faces
                         if face_recognition.compare_faces([known_face], unknown_face)]
-    res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'".format(TOKEN, chat_id,
+    res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(TOKEN, chat_id,
                                                                                                ACCEPT_MESSAGE
-                                                                                               if len(match_results) >= 1 else DECLINE_MESSAGE))
+                                                                                               if match_results else DECLINE_MESSAGE))
     return Response("success")
 
 
 if __name__ == '__main__':
-    app.run(port=5002)
+    waitress.serve(app, port="5002")
