48bdac2d829bbd6b5fec943cc45ff05d10fed079
eindor-cloud <eindor13@gmail.com>
2022-08-21 09:46:14 +0300

    working recognition

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, Response, request
 import requests
-import commands
+import face_recognition
 
 app = Flask(__name__)
 TOKEN = ''
-NGROK = 'https://302a-82-80-173-170.eu.ngrok.io'
-TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url={}/message'.format(
-    TOKEN, NGROK)
+NGROK = 'https://bab6-82-80-173-170.eu.ngrok.io'
+TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url={}/message'.format(TOKEN, NGROK)
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
 
 
@@ -20,12 +19,19 @@ def handle_message():
     json = request.get_json()
     chat_id = json['message']['chat']['id']
     message = json['message']['text']
-    message = message.split()
-    res = commands.COMMANDS.get(message[0])(message[1])
-    requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-                 .format(TOKEN, chat_id, res))
+    res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'".format(TOKEN, chat_id, message))
     return Response("success")
 
 
 if __name__ == '__main__':
     app.run(port=5002)
+    import face_recognition
+
+    known_image = face_recognition.load_image_file("resources/4.jpg")
+    unknown_image = face_recognition.load_image_file("resources/2.jpg")
+
+    biden_encoding = face_recognition.face_encodings(known_image)[0]
+    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+    results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+    print(results)
