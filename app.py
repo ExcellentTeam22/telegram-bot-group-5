28c0d79d394e745403297cd3920e9196b7c285c0
idanbau <81595179+idanbau@users.noreply.github.com>
2022-08-21 11:26:18 +0300

    download picture, fail to load it to face recognition

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import requests
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-TOKEN = ''
-NGROK = 'https://bab6-82-80-173-170.eu.ngrok.io'
+TOKEN = '5696168580:AAHuXW8Jd0lZjYROK4cgpPEfEsOXhOxbEOE'
+NGROK = 'https://0f55-82-80-173-170.eu.ngrok.io'
 TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url={}/message'.format(TOKEN, NGROK)
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
 
@@ -20,12 +20,16 @@ def handle_message():
     print("got message")
     json = request.get_json()
     chat_id = json['message']['chat']['id']
-    message = json['message']['text']
-
+    message = json['message']['photo']
+    print(json)
+    print('https://api.telegram.org/bot{}/getfile?file_id={}'.format(TOKEN, message[0]['file_id']))
+    pic = requests.get('https://api.telegram.org/bot{}/getfile?file_id={}'.format(TOKEN, message[0]['file_id']))
+    pic_file_path = pic.json()['result']['file_path']
+    downloaded_pic = requests.get('https://api.telegram.org/file/bot{}/{}'.format(TOKEN, pic_file_path)).content
     known_face = face_recognition.face_encodings(face_recognition.load_image_file("resources/4.jpg"))[0]
-    unknown_faces = face_recognition.face_encodings(message)
-    match_results = face_recognition.compare_faces(unkown_face for unkown_face in unknown_faces
-                                                   if face_recognition.compare_faces([known_face], unkown_face))
+    unknown_faces = face_recognition.face_encodings(face_recognition.load_image_file(downloaded_pic))
+    match_results = any(unknown_face for unknown_face in unknown_faces
+                        if face_recognition.compare_faces([known_face], unknown_face))
     res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'".format(TOKEN, chat_id,
                                                                                                "ANI 0-5-2-5-3-8-1-6-4-8"
                                                                                                if match_results else "MI ZOT?!"))
