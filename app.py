80824afea93fcff248ba36e74dcca2680391fce1
idanbau <81595179+idanbau@users.noreply.github.com>
2022-08-21 10:28:16 +0300

    Face detection, connected to bot

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, Response, request
-import requests
 import face_recognition
+import requests
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 TOKEN = ''
@@ -19,19 +21,22 @@ def handle_message():
     json = request.get_json()
     chat_id = json['message']['chat']['id']
     message = json['message']['text']
-    res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'".format(TOKEN, chat_id, message))
+
+    known_face = face_recognition.face_encodings(face_recognition.load_image_file("resources/4.jpg"))[0]
+    unknown_faces = face_recognition.face_encodings(message)
+    match_results = face_recognition.compare_faces(unkown_face for unkown_face in unknown_faces
+                                                   if face_recognition.compare_faces([known_face], unkown_face))
+    res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'".format(TOKEN, chat_id,
+                                                                                               "ANI 0-5-2-5-3-8-1-6-4-8"
+                                                                                               if match_results else "MI ZOT?!"))
     return Response("success")
 
 
 if __name__ == '__main__':
     app.run(port=5002)
-    import face_recognition
 
-    known_image = face_recognition.load_image_file("resources/4.jpg")
-    unknown_image = face_recognition.load_image_file("resources/2.jpg")
+    # biden_encoding = face_recognition.face_encodings(known_image)[0]
+    # unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-    biden_encoding = face_recognition.face_encodings(known_image)[0]
-    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-
-    results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
-    print(results)
+    # results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+    # print(results)
